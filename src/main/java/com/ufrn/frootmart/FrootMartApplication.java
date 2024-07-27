f7d05966b58f01bed66884bd31ee71951605ccd5
estelitafernanda <estelitabrito777@gmail.com>
2024-07-27 10:07:41 -0300

    commit initial

---
+++ src/main/java/com/ufrn/frootmart/FrootMartApplication.java
@@ -0,0 +1,13 @@
+package com.ufrn.frootmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrootMartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrootMartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ufrn/frootmart/FrootMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufrn.frootmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrootMartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
